002621c392036e6f7fce850e9ab1d769ed9f48ab
linde12 <lindeoscar@gmail.com>
2020-07-15 17:46:02 +0200

    fix: indirect loads

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,19 +1,18 @@
-use crate::mmu::Mmu;
 use crate::error::GBError;
+use crate::mmu::Mmu;
 
 #[derive(Debug)]
 pub enum ProgramCounter {
     Next,
-    Jump(usize)
+    Jump(usize),
 }
 
 #[derive(Debug)]
 pub enum Operand {
-    Direct(u16),
-    Indirect(Register),
+    Direct(Register), // direct addressing, take the value of the register
+    Indirect(Register), // indirect addressing, take the value at the address the register is pointing to
     Byte(u8),
     Word(u16),
-    Register(Register),
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
@@ -96,314 +95,317 @@ impl Cpu {
             0x01 => {
                 let value = self.mmu.word(self.pc)?;
                 Ok(Op::LD(
-                    Operand::Register(Register::BC),
+                    Operand::Direct(Register::BC),
                     Operand::Word(value),
                 ))
             }
 
             0x06 => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::B), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::B), Operand::Byte(value)))
             }
 
             0x0E => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::C), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::C), Operand::Byte(value)))
             }
 
             0x16 => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::D), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::D), Operand::Byte(value)))
             }
 
             0x1E => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::E), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::E), Operand::Byte(value)))
             }
 
             0x26 => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::H), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::H), Operand::Byte(value)))
             }
 
             0x2E => {
                 let value = self.mmu.byte(self.pc)?;
-                Ok(Op::LD(Operand::Register(Register::L), Operand::Byte(value)))
+                Ok(Op::LD(Operand::Direct(Register::L), Operand::Byte(value)))
             }
 
             // LD A, x
             0x7F => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::A),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::A),
             )),
             0x78 => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::B),
             )),
             0x79 => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::C),
             )),
             0x7A => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::D),
             )),
             0x7B => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::E),
             )),
             0x7C => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::H),
             )),
             0x7D => Ok(Op::LD(
-                Operand::Register(Register::A),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::A),
+                Operand::Direct(Register::L),
             )),
-            0x7E => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::A), Operand::Word(value)))
-            }
+            0x7E => Ok(Op::LD(
+                Operand::Direct(Register::A),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD B, x
             0x40 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::B),
             )),
 
             0x41 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::C),
             )),
 
             0x42 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::D),
             )),
 
             0x43 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::E),
             )),
 
             0x44 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::H),
             )),
 
             0x45 => Ok(Op::LD(
-                Operand::Register(Register::B),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::B),
+                Operand::Direct(Register::L),
             )),
 
-            0x46 => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::B), Operand::Word(value)))
-            }
+            0x46 => Ok(Op::LD(
+                Operand::Direct(Register::B),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD C, x
             0x48 => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::B),
             )),
             0x49 => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::C),
             )),
             0x4A => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::D),
             )),
             0x4B => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::E),
             )),
             0x4C => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::H),
             )),
             0x4D => Ok(Op::LD(
-                Operand::Register(Register::C),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::C),
+                Operand::Direct(Register::L),
             )),
-            0x4E => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::C), Operand::Word(value)))
-            }
+            0x4E => Ok(Op::LD(
+                Operand::Direct(Register::C),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD D, x
             0x50 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::B),
             )),
 
             0x51 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::C),
             )),
 
             0x52 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::D),
             )),
 
             0x53 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::E),
             )),
 
             0x54 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::H),
             )),
 
             0x55 => Ok(Op::LD(
-                Operand::Register(Register::D),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::D),
+                Operand::Direct(Register::L),
             )),
 
-            0x56 => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::D), Operand::Word(value)))
-            }
+            0x56 => Ok(Op::LD(
+                Operand::Direct(Register::D),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD E, x
             0x58 => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::B),
             )),
             0x59 => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::C),
             )),
             0x5A => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::E),
             )),
             0x5B => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::E),
             )),
             0x5C => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::H),
             )),
             0x5D => Ok(Op::LD(
-                Operand::Register(Register::E),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::E),
+                Operand::Direct(Register::L),
             )),
-            0x5E => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::E), Operand::Word(value)))
-            }
+            0x5E => Ok(Op::LD(
+                Operand::Direct(Register::E),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD H, x
             0x60 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::B),
             )),
 
             0x61 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::C),
             )),
 
             0x62 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::D),
             )),
 
             0x63 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::E),
             )),
 
             0x64 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::H),
             )),
 
             0x65 => Ok(Op::LD(
-                Operand::Register(Register::H),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::H),
+                Operand::Direct(Register::L),
             )),
 
-            0x66 => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::H), Operand::Word(value)))
-            }
+            0x66 => Ok(Op::LD(
+                Operand::Direct(Register::H),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD L, x
             0x68 => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::B),
             )),
 
             0x69 => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::C),
             )),
 
             0x6a => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::D),
             )),
 
             0x6b => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::E),
             )),
 
             0x6c => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::H),
             )),
 
             0x6d => Ok(Op::LD(
-                Operand::Register(Register::L),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::L),
+                Operand::Direct(Register::L),
             )),
 
-            0x6e => {
-                let value = self.mmu.word(self.hl() as usize)?;
-                Ok(Op::LD(Operand::Register(Register::L), Operand::Word(value)))
-            }
+            0x6e => Ok(Op::LD(
+                Operand::Direct(Register::L),
+                Operand::Indirect(Register::HL),
+            )),
 
             // LD (HL), x
             0x70 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::B),
+                Operand::Direct(Register::B),
             )),
 
             0x71 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::C),
+                Operand::Direct(Register::C),
             )),
 
             0x72 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::D),
+                Operand::Direct(Register::D),
             )),
 
             0x73 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::E),
+                Operand::Direct(Register::E),
             )),
 
             0x74 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::H),
+                Operand::Direct(Register::H),
             )),
 
             0x75 => Ok(Op::LD(
                 Operand::Indirect(Register::HL),
-                Operand::Register(Register::L),
+                Operand::Direct(Register::L),
             )),
 
             // LD SP, $nn
             0x31 => {
-                let value = self.mmu.word(self.pc+1)?;
-                Ok(Op::LD(Operand::Register(Register::SP), Operand::Direct(value)))
+                let value = self.mmu.word(self.pc + 1)?;
+                Ok(Op::LD(
+                    Operand::Direct(Register::SP),
+                    Operand::Word(value),
+                ))
             }
 
             _ => Err(GBError::UnknownOperation(op)),
